b93d6412edd051cb49cce64cebdd1705e4dc4bb6
dockerlead <dockerlead@mail.com>
2021-03-17 21:32:10 -0500

    :sparkles:Add filter pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,6 +32,8 @@ export class AppComponent {
       started: new Date(2021, 0, 15)
     }
   ];
+  filteredStatus='';
+
   getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
     return {
       'list-group-item-success': server.status === 'stable',
